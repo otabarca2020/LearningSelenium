3fb53600e95da49767c662dc5deff84f0d97d6d4
otabarca2020 <71529681+otabarca2020@users.noreply.github.com>
2022-08-18 11:31:24 -0600

    Add files via upload

---
+++ Day4/DeserializeResponse.java
@@ -0,0 +1,20 @@
+package APITest;
+
+public class DeserializeResponse {
+
+	public int id;
+    public String token;
+    
+    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
